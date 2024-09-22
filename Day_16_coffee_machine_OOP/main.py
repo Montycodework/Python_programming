d4e585da5038da3a211c63df39bb51b54c22f940
Monty <montyfelo.mm@gmail.com>
2024-09-22 17:47:05 +0530

    Day_16

---
+++ Day_16_coffee_machine_OOP/main.py
@@ -0,0 +1,34 @@
+from menu import Menu
+from Coffee_maker import CoffeeMaker
+from Money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+
+
+
+# ___________HOME WORK____________
+
+# Prepare for class and object excercise
+
+# Create a Temprature class. Make two methods :
+# 1. convertFahrenheit - It will take celsius and will print it into Fahrenheit.
+# 2. convertCelsius - It will take Fahrenheit and will convert it into Celsius.
