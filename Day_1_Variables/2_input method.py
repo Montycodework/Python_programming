4579bb257c6a369a6565f259ee0dac7ea716ba82
Montycodework <montyfelo.mm@gmail.com>
2024-09-23 12:45:37 +0530

    improvised the comments

--- Day_1_Variables/2_input method.py
+++ Day_1_Variables/2_input method.py
@@ -1,15 +1,13 @@
+# --------Input_function--------
+
 input("What is your name? ")
+
+# -----------len function------------
 
 print( len("The string here you wanted to count the length of its caharcters")) 
 # len() function is use to find out the length of the characters of string
 
+# ------------len+input function-----------
+
 print( len( input("What is your name? ")))
 # Concatinating these functions to perform the task to find out the characters length enter by the user and print it.
-
-
-# _________________________________________________________
-# Highlights of this session
-
-# Input function
-# len function
-# len + input
